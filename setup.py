30f6a8242d7bb00a0712c6d8d7928280eafca55f
Philip Bergen <pbergen@salesforce.com>
2014-08-01 16:15:56 -0400

    Add RST to long description.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name = "salvus",
-    version = "0.1",
+    version = "0.1.1",
     py_modules = ['salvus'],
     author = "philipbergen",
     author_email = "philipbergen at gmail com",
     description = "In-memory credential store with yubikey auth",
+    long_description=open('README.rst').read(),
     license = "MIT",
     keywords = "yubikey auth",
     install_requires=[
